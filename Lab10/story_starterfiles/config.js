a6da9ef78d038747fa5d7100a076e90a07f6da4e
yk539 <yjan2013@gmail.com>
2022-10-29 17:05:18 -0400

    Update config.js

--- Lab10/story_starterfiles/config.js
+++ Lab10/story_starterfiles/config.js
@@ -78,8 +78,8 @@ var config = {
             description: 'Around 6,000 people live in Red Hook Houses, the largest NYCHA development in Brooklyn. This design and research strategy included community involvement at every stage using focus groups, interviews, surveys, and workshops, assuring the creation of a successful master plan that safeguarded and strengthened the community. Passive barriers, which automatically deploy in the event of high water, are added to the occupiable flood wall. ',
             location: {
                 center: [-74.00798328521597, 40.67599367612261],
-                zoom: 18,
-                pitch: 50,
+                zoom: 15,
+                pitch: 60,
                 bearing: -43.2
             },
             mapAnimation: 'flyTo',
